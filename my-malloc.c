e152a13263d04c5123da3793a933c3a76ec543e6
Khoi Huynh <khoihuynh2212@gmail.com>
2026-06-28 12:34:50 -0500

    Implement calloc and realloc functions. Add strict alignment, correct pointer arithmetic on footer

--- my-malloc.c
+++ my-malloc.c
@@ -1,12 +1,11 @@
 #define _GNU_SOURCE
 #include <unistd.h>
 #include <stdio.h>
-#include <assert.h>
 #include "list.h"
 
 #define ALIGN _Alignof(max_align_t)
 #define ALIGN_UP(n) (((n) + ALIGN - 1) & ~(ALIGN - 1))
-#define MIN_FREE_BLOCK (HEADER_SIZE + sizeof(size_t) + ALIGN)
+#define MIN_FREE_BLOCK (HEADER_SIZE + FOOTER_SIZE + ALIGN)
 #define FREE 1
 #define ALLOCATED 0
 
@@ -17,17 +16,20 @@ typedef struct Block
     list list;
 } Block;
 
-const size_t HEADER_SIZE = sizeof(Block);
+#define HEADER_SIZE (sizeof(Block))
+#define FOOTER_SIZE ALIGN_UP(sizeof(size_t))
 #define HEAP_SIZE 128 * 1024 + HEADER_SIZE
 
 #define BLOCK_NEXT_HEADER(curr, payload) \
-    ((Block *)((char *)((curr) + 1) + (payload) + sizeof(size_t)))
+    ((Block *)((char *)((curr) + 1) + (payload) + FOOTER_SIZE))
 
 #define BLOCK_PREV_HEADER(curr, prev_size) \
-    ((Block *)((char *)curr - sizeof(size_t) - prev_size - HEADER_SIZE))
+    ((Block *)((char *)curr - FOOTER_SIZE - prev_size - HEADER_SIZE))
 
 // set function prototypes
 void set_footer(Block *block);
+void free(void *ptr);
+bool try_expand(Block *block, size_t newPayload);
 
 static Block head = {
     .payload = 0,
@@ -36,6 +38,7 @@ static Block head = {
 
 static void *heap_start;
 static void *heap_end;
+
 void heap_init()
 {
 
@@ -49,13 +52,15 @@ void heap_init()
     }
     Block *first = (Block *)start;
 
-    printf("The heap is start at %p\n", start);
-    first->payload = HEAP_SIZE - HEADER_SIZE - sizeof(size_t);
+    printf("The heap is start at %p\n", heap_start);
+    printf("The heap is end at %p\n", heap_end);
+    size_t raw_payload = HEAP_SIZE - HEADER_SIZE - FOOTER_SIZE;
+
+    first->payload = raw_payload & ~(ALIGN - 1);
     first->free = FREE;
     set_footer(first);
     list_init(&first->list);
     list_add_after(&head.list, &first->list);
-
 }
 
 Block *find_suitable_block(size_t requestSize)
@@ -78,7 +83,7 @@ Block *requestBlock(size_t size)
 {
 
     Block *newBlock = NULL;
-    void *request = sbrk(HEADER_SIZE + size);
+    void *request = sbrk(HEADER_SIZE + size + FOOTER_SIZE);
     if (request == (void *)-1)
     {
         return NULL;
@@ -90,14 +95,14 @@ Block *requestBlock(size_t size)
     set_footer(newBlock);
     list_init(&newBlock->list);
 
-    heap_end = (char*)request + HEADER_SIZE + size;
+    heap_end = (char *)request + HEADER_SIZE + size + FOOTER_SIZE;
     return newBlock;
 }
 
 Block *split(Block *block, size_t requestPayload)
 {
     Block *remainder = BLOCK_NEXT_HEADER(block, requestPayload);
-    remainder->payload = block->payload - requestPayload - HEADER_SIZE;
+    remainder->payload = block->payload - requestPayload - HEADER_SIZE - FOOTER_SIZE;
     remainder->free = FREE;
     set_footer(remainder);
     list_init(&remainder->list);
@@ -117,14 +122,14 @@ void coalesce(Block *curr)
 
     Block *next_block = BLOCK_NEXT_HEADER(curr, curr->payload);
 
-    if ((char*) next_block != (char*) heap_end && next_block->free == FREE)
+    if ((char *)next_block < (char *)heap_end && next_block->free == FREE)
     {
-        curr->payload += HEADER_SIZE + next_block->payload + sizeof(size_t);
+        curr->payload += HEADER_SIZE + next_block->payload + FOOTER_SIZE;
         set_footer(curr);
         list_unlink(&next_block->list);
     }
 
-    size_t *footer = (size_t *)((char *)curr - sizeof(size_t));
+    size_t *footer = (size_t *)((char *)curr - FOOTER_SIZE);
 
     if ((char *)footer >= (char *)heap_start)
     {
@@ -132,22 +137,20 @@ void coalesce(Block *curr)
 
         if ((char *)prev_block >= (char *)heap_start && prev_block->free == FREE)
         {
-            prev_block->payload += HEADER_SIZE + sizeof(size_t) + curr->payload;
+            prev_block->payload += HEADER_SIZE + FOOTER_SIZE + curr->payload;
             set_footer(prev_block);
             list_unlink(&curr->list);
         }
     }
 }
-
-// set payload footer
 void set_footer(Block *block)
 {
     size_t *footer =
-        (size_t *)((char *)(block + 1) + block->payload) - 1;
+        (size_t *)((char *)(block + 1) + block->payload);
 
     *footer = block->payload;
 }
-void *my_malloc(size_t size)
+void *malloc(size_t size)
 {
 
     if (size == 0)
@@ -165,25 +168,106 @@ void *my_malloc(size_t size)
         {
             return NULL;
         }
-        printf("Size of allocated block after requested new block %zu\n", block->payload);
-        printf("-----------------------------------\n");
         return block + 1;
     }
 
     if (block->payload >= requestPayload + MIN_FREE_BLOCK)
     {
         block = split(block, requestPayload);
-        printf("Size of allocated block after spliting %zu\n", block->payload);
-        printf("-----------------------------------\n");
         return block + 1;
     }
     list_unlink(&block->list); // only unlink if it came from free list
     block->free = ALLOCATED;
-    printf("Size of allocated block if found %zu\n", block->payload);
-    printf("-----------------------------------\n");
     return block + 1;
 }
-void my_free(void *ptr)
+void *calloc(size_t num, size_t size)
+{
+    if (num != 0 && size > __SIZE_MAX__ / num)
+    {
+        return NULL;
+    }
+
+    void *ptr = malloc(num * size);
+    if (ptr == NULL)
+    {
+        return NULL;
+    }
+
+    memset(ptr, 0, num * size);
+    return ptr;
+}
+
+bool try_expand(Block *curr, size_t newPayload)
+{
+    Block *next = BLOCK_NEXT_HEADER(curr, curr->payload);
+
+    if ((char *)next >= (char *)heap_end)
+        return false;
+
+    if (next->free == ALLOCATED)
+        return false;
+
+    size_t merge_size = next->payload + curr->payload + HEADER_SIZE + FOOTER_SIZE;
+    if (merge_size < newPayload)
+    {
+        return false;
+    }
+    else
+    {
+        curr->payload = merge_size;
+        set_footer(curr);
+        list_unlink(&next->list);
+        return true;
+    }
+}
+
+void *realloc(void *ptr, size_t size)
+{
+    if (ptr == NULL)
+    {
+        return malloc(size);
+    }
+    if (size == 0)
+    {
+        my_free(ptr);
+        return NULL;
+    }
+
+    size_t newPayload = ALIGN_UP(size);
+
+    Block *block = (Block *)ptr - 1;
+
+    // shrink
+    if (newPayload <= block->payload)
+    {
+        if (block->payload >= newPayload + MIN_FREE_BLOCK)
+            split(block, newPayload);
+        
+        return ptr;
+        
+    }
+    // grow
+    if (try_expand(block, newPayload))
+    {
+        if (block->payload >= newPayload + MIN_FREE_BLOCK)
+            split(block, newPayload);
+        
+        return ptr;
+    } 
+
+    void* new_ptr = malloc(newPayload);
+    if(new_ptr == NULL) return NULL;
+
+    size_t copySize = block->payload;
+    if(copySize > newPayload) 
+        copySize = newPayload;
+
+    memcpy(new_ptr, ptr, copySize);
+    my_free(ptr);
+    return new_ptr;
+}
+
+void free(void *ptr)
 {
     if (ptr == NULL)
         return;
@@ -206,18 +290,21 @@ int main()
     printf("First free block size is %zu\n", blk->payload);
     printf("-----------------------------------\n");
 
-    int *y = my_malloc(sizeof(int));
+    int *y = malloc(sizeof(int));
     // printf("Allocated Y: %zu blocks\n", list_length(&head.list));
     // printf("-----------------------------------\n");
     printf("Address of y is at %p\n", y);
-    int *x = my_malloc(sizeof(int));
+    int *x = malloc(sizeof(int));
     // printf("Allocated X: %zu blocks\n", list_length(&head.list));
     // printf("-----------------------------------\n");
     printf("Address of x is at %p\n", x);
-    double *z = my_malloc(sizeof(double));
+    double *z = malloc(sizeof(double));
     // printf("Allocated Z: %zu blocks\n", list_length(&head.list));
     // printf("-----------------------------------\n");
     printf("Address of z is at %p\n", z);
+
+    // char* name = my_malloc(100000000);
+    //  printf("Address of name is at %p\n", name);
 
     my_free(y);
     my_free(x);
