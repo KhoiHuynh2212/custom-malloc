d59386cfc8f9a38e5cb2089ea09d95c6e801a3ab
KhoiHuynh2212 <khoihuynh2212@gmail.com>
2026-08-16 01:04:41 -0500

    write regression test about find_suitable_block new algorithm. ALL PASS !

--- test/test_bugs.c
+++ test/test_bugs.c
@@ -1,6 +1,5 @@
 #include "debug.h"
-#include "malloc.h"
-
+#include "my-malloc.h"
 
 void test_grow_top_topsize_stays_synced(void)
 {
@@ -8,11 +7,10 @@ void test_grow_top_topsize_stays_synced(void)
 
     size_t almost_all = 65526;
     void *a = my_malloc(almost_all);
-    check_top_chunk();   
+    check_top_chunk();
 
-    
     void *b = my_malloc(500);
-    check_top_chunk();   
+    check_top_chunk();
 
     printf("test_grow_top_topsize_stays_synced: PASS\n");
 }
@@ -21,18 +19,18 @@ void test_top_carve_payload_matches_request(void)
 {
     heap_init();
     void *p = my_malloc(200);
-    check_heap();  
+    check_heap();
     printf("test_top_carve_payload_matches_request: PASS\n");
 }
 
-
-void test_no_shrink_below_threshold (void) {
+void test_no_shrink_below_threshold(void)
+{
     heap_init();
-    const malloc_state * s = debug_get_state();
-    size_t top_size_before = s->topsize;  
-    void* heap_end_before = s->heap_end;
-    assert(top_size_before < (size_t) SHRINK_THRESHOLD);
-    void* p = my_malloc(16);
+    const malloc_state *s = debug_get_state();
+    size_t top_size_before = s->topsize;
+    void *heap_end_before = s->heap_end;
+    assert(top_size_before < (size_t)SHRINK_THRESHOLD);
+    void *p = my_malloc(16);
     assert(p != NULL);
     my_free(p);
     assert(s->heap_end == heap_end_before);
@@ -40,15 +38,15 @@ void test_no_shrink_below_threshold (void) {
     assert(s->topsize >= top_size_before);
     printf("test_no_shrink_below_threshold: PASS (topsize=%zu, still below %d)\n",
            s->topsize, SHRINK_THRESHOLD);
-} 
+}
 
-
-void test_shrink_lands_on_pad_size(void) {
+void test_shrink_lands_on_pad_size(void)
+{
     heap_init();
-    const malloc_state * st = debug_get_state(); 
+    const malloc_state *st = debug_get_state();
 
     for (int i = 0; i < 3; i++)
-        assert(grow_top(1) != NULL); 
+        assert(grow_top(1) != NULL);
     assert(st->topsize >= (size_t)SHRINK_THRESHOLD);
 
     void *guard = my_malloc(16);
@@ -61,49 +59,121 @@ void test_shrink_lands_on_pad_size(void) {
 
     printf("test_no_shrink_below_threshold: PASS (topsize=%zu, still below %d)\n",
            st->topsize, SHRINK_THRESHOLD);
-
 }
 
-
-void test_descending_order_bin(void) {
+void test_descending_order_bin(void)
+{
     heap_init();
 
     const malloc_state *st = debug_get_state();
 
-    void* p = my_malloc(2208);
-    void* sp1 = my_malloc(64);
-    void* a = my_malloc(3001);
-    void* sp2 = my_malloc(64);
-    void* b = my_malloc(3234);
-    void* sp3 = my_malloc(64);
+    void *p = my_malloc(2208);
+    assert(p != NULL);
 
-   
+    void *sp1 = my_malloc(64);
+    assert(sp1 != NULL);
+
+    void *a = my_malloc(3001);
+    assert(a != NULL);
+
+    void *sp2 = my_malloc(64);
+    assert(sp2 != NULL);
+
+    void *b = my_malloc(3234);
+    assert(b != NULL);
+
+    void *sp3 = my_malloc(64);
+    assert(sp3 != NULL);
+
+    void *c = my_malloc(2267);
+    assert(c != NULL);
+    void *sp4 = my_malloc(64);
+    assert(sp4 != NULL);
+
+    void *d = my_malloc(2943);
+    assert(d != NULL);
+    void *sp5 = my_malloc(64);
+    assert(sp5 != NULL);
+
+    long g_scan_before = g_scan_steps;
+    printf("Scan before is %ld\n", g_scan_before);
     int idx1 = get_bin_bucket(2208);
-    int idx2 = get_bin_bucket(2103);
-    int idx3 = get_bin_bucket(2304);
-
-    printf("Index 1 is %d\n", idx1);
-    printf("Index 2 is %d\n", idx2);
-    printf("Index 3 is %d\n", idx3);
+    assert(idx1 == get_bin_bucket(2208));
 
     my_free(p);
     my_free(a);
     my_free(b);
+    my_free(c);
+    my_free(d);
 
     check_heap_bin_consistency();
-    
 
-    const list* head = &st->bins[idx1];\
-    assert(list_length(head) == 3);
-    list* curr = head->next;
-    do {
-        mblockptr * curr_block = list_entry(curr, mblockptr, list);
-        size_t size = curr_block->payload;
-        list* next = curr->next;
-        printf("Value is %d\n", (int)size);
-        curr = curr->next;
-    } while(curr != head);
+    const list *head = &st->bins[idx1];
+    assert(list_length(head) == 5);
+    list *curr = head->next;
+    do
+    {
+        mblockptr *curr_block = list_entry(curr, mblockptr, list);
+        size_t curr_size = curr_block->payload;
+        list *next = curr->next;
+        if (next != head)
+        {
+            mblockptr *next_block = list_entry(next, mblockptr, list);
+            size_t next_size = next_block->payload;
+            assert(curr_size >= next_size);
+        }
+        curr = next;
+    } while (curr != head);
+
+    void *x = my_malloc(3008);
+
+    long g_scan_after = g_scan_steps;
+    printf("Scan after is %ld\n", g_scan_after);
+
+    my_free(x);
+    assert(g_scan_steps - g_scan_before <= 3);
+
+    my_free(sp1);
+    my_free(sp2);
+    my_free(sp3);
+    my_free(sp4);
+    my_free(sp5);
+
+    void *dup1 = my_malloc(2560);
+    assert(dup1 != NULL);
+
+    void *sp6 = my_malloc(64);
+    assert(sp6 != NULL);
+
+    void *dup2 = my_malloc(2560);
+    assert(dup2 != NULL);
+
+    void *sp7 = my_malloc(64);
+    assert(sp7 != NULL);
+
+    int idx2 = get_bin_bucket(2560);
+
+    my_free(dup1);
+    my_free(dup2);
+
+    check_heap_bin_consistency();
+
+    const list *head2 = &st->bins[idx2];
+    assert(list_length(head2) == 2);
+
+    mblockptr *dup1_block = (mblockptr *)dup1 - 1;
+    mblockptr *dup2_block = (mblockptr *)dup2 - 1;
+
+    mblockptr *first_in_bin = list_entry(head2->next, mblockptr, list);
+    mblockptr *second_in_bin = list_entry(head2->next->next, mblockptr, list);
+
+    assert(first_in_bin == dup1_block);
+    assert(second_in_bin == dup2_block);
+
+    my_free(sp6);
+    my_free(sp7);
 }
-int main() {
+int main()
+{
     test_descending_order_bin();
 }
